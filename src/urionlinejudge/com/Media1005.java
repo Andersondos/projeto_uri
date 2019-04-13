4c9609c7a815a1ccea0481d693e6ca37654ba338
Anderson Douglas de Oliveira Santos <anderson.santos@medraleng.local>
2019-04-12 21:04:14 -0300

    Teste

---
+++ src/urionlinejudge/com/Media1005.java
@@ -0,0 +1,17 @@
+package urionlinejudge.com;
+
+import java.util.*;
+
+public class Media1005 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		double a = scan.nextDouble();
+		double b = scan.nextDouble();
+		
+		double resu = ((a*3.5) + (b*7.5)) / 11;
+		
+	System.out.printf("MEDIA = %.5f\n",resu);
+ }
+}
